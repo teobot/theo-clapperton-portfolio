7452edd49a9ccd2483f657f6926a4bf7e296baa4
theo.clapperton <theoclapperton@outlook.com>
2022-08-21 22:48:34 +0100

    updated

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,16 +1,16 @@
 import React from "react";
 
+import { BsGithub } from "react-icons/bs";
+
 export default function ProjectCard({
   title = "",
   desc = "",
   img = "linear-gradient(90deg, #74EBD5 0%, #9FACE6 100%)",
-  link = "",
   github = "",
   live = "",
-  stack = "",
 }) {
   return (
-    <div className="d-flex flex-column p-1 justify-content-between mb-5">
+    <div className="d-flex flex-column p-1 justify-content-between mb-5 position-relative w-100">
       <div
         style={{
           borderRadius: 5,
@@ -33,13 +33,15 @@ export default function ProjectCard({
         {title}
       </div>
       <div
-        className="mt-4"
+        className="mt-4 w-100"
         style={{
           fontWeight: "500",
           fontSize: "1rem",
           color: "#747474",
           height: 100,
           maxHeight: 100,
+          overflowWrap: "break-word",
+          wordBreak: "break-word",
         }}
       >
         {desc}
@@ -50,14 +52,19 @@ export default function ProjectCard({
         }}
         className="d-flex flex-row justify-content-center align-center pt-2 pb-2 mt-5"
         style={{
+          ...(github ? {} : { visibility: "hidden" }),
           background: "linear-gradient(90deg, #6A85B6 0%, #BAC8E0 100%)",
           borderRadius: 5,
           width: 160,
           height: 40,
           fontWeight: "700",
           cursor: "pointer",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
         }}
       >
+        <BsGithub size={24} className="me-2" />
         View Source
       </div>
       <div
@@ -66,6 +73,7 @@ export default function ProjectCard({
         }}
         className="mt-4"
         style={{
+          ...(live ? {} : { visibility: "hidden" }),
           fontWeight: "500",
           textDecoration: "underline",
           color: "#868686",
--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,26 @@ export default function Home({ projects }) {
         <Header>i like to build</Header>
 
         <Row className="w-100 mt-4">
-          {projects.map((project, index) => {
-            return (
-              <Col
-                key={index + project.title}
-                md={6}
-                sm={12}
-                lg={4}
-                className="d-flex justify-content-center"
-              >
-                <ProjectCard {...project} />
-              </Col>
-            );
-          })}
+          {projects
+            .sort((a, b) => {
+              let aOrder = a.order || 99;
+              let bOrder = b.order || 99;
+              return aOrder - bOrder;
+            })
+            .map((project, index) => {
+              if (project.hidden) return null;
+              return (
+                <Col
+                  key={index + project.title}
+                  md={6}
+                  sm={12}
+                  lg={4}
+                  className="d-flex justify-content-center"
+                >
+                  <ProjectCard {...project} />
+                </Col>
+              );
+            })}
         </Row>
       </Container>
     </Container>
@@ -40,6 +47,7 @@ export async function getServerSideProps(context) {
       img: "https://github.com/teobot/bucket/blob/main/portfolio/toffyWide.png?raw=true",
       live: "https://toffy.netlify.app/",
       github: "https://github.com/teobot/toffy",
+      order: 2,
     },
     {
       title: "Estr",
@@ -47,76 +55,64 @@ export async function getServerSideProps(context) {
       img: "https://github.com/teobot/bucket/blob/main/portfolio/ester-min.png?raw=true",
       github: "https://github.com/teobot/ester-frontend",
       live: "https://estr.netlify.app/",
+      order: 2,
     },
     {
-      title: "Shark.net",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/ester-min.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
+      title: "Shark.Net",
+      desc: "This is a paid commission to build a web application for the Manchester Sharks Waterpolo team website. It includes a CMS for managing Users, Blogs, Contact Messages and page content.",
+      img: "https://github.com/teobot/bucket/blob/main/portfolio/shark.net-min.png?raw=true",
+      live: "https://www.manchester-sharks.co.uk/",
+      order: 1,
     },
     {
       title: "ACNH App",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/ester-min.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
+      desc: "A small app designed for mobile that allows you to view the prices of ACNH animal items for the game.",
+      img: "https://github.com/teobot/bucket/blob/main/portfolio/acnh-min.png?raw=true",
+      github: "https://github.com/teobot/acnh-animal-helper",
+      live: "https://teobot-anch-priceguide.netlify.app/",
+      order: 99,
+      hidden: true,
     },
     {
       title: "2048",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/ester-min.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
+      desc: "This is a simple take on the classic game of 2048. It was built in less than an hour as a challenge.",
+      img: "https://github.com/teobot/bucket/blob/main/portfolio/2048-min.png?raw=true",
+      github: "https://github.com/teobot/2048",
+      live: "https://teobot-2048.netlify.app/",
+      order: 99,
+      hidden: true,
     },
     {
       title: "WDrill",
       desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
-    },
-    {
-      title: "Wordle",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
+      img: "https://github.com/teobot/bucket/blob/main/portfolio/wdrill-min.png?raw=true",
+      github: "https://github.com/teobot/wdrill",
+      live: "https://wdrill.vercel.app/",
+      order: 99,
+      hidden: true,
     },
     {
       title: "Zester",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
+      desc: "Zester is a web application for testing system, you can create items, track progress and get updates when tests are done.",
+      img: "https://github.com/teobot/bucket/blob/main/portfolio/zester-min.png?raw=true",
+      github: "https://github.com/teobot/zester-front-end",
+      live: "https://zester.netlify.app/",
+      order: 4,
     },
     {
       title: "Pretty Screenshots",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
+      desc: "A typescript app for getting pretty screenshots for README files",
+      img: "https://github.com/teobot/bucket/blob/main/portfolio/pretytyscxen-min.png?raw=true",
+      github: "https://github.com/teobot/pretty-sceenshots",
+      live: "https://prettier-screenshots.netlify.app/",
+      order: 99,
+      hidden: true,
     },
     {
       title: "Support System",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
-    },
-    {
-      title: "Phase 10 Tracker",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
-    },
-    {
-      title: "placeholder",
-      desc: "Placeholder",
-      img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
-      github: "Placeholder",
-      live: "Placeholder",
+      desc: "Developed for use in the Timpson Development team it allows you to track tickets from phone calls and other sources.",
+      img: "https://github.com/teobot/bucket/blob/main/portfolio/ticket%20support-min.png?raw=true",
+      order: 7,
     },
     {
       title: "Coffida",
@@ -124,6 +120,7 @@ export async function getServerSideProps(context) {
       img: "https://github.com/teobot/bucket/blob/main/portfolio/coffida2.png?raw=true",
       github: "https://github.com/teobot/18055445-coffida-mobile-app",
       live: "",
+      order: 3,
     },
     {
       title: "Type in Cat",
@@ -131,34 +128,35 @@ export async function getServerSideProps(context) {
       img: "https://github.com/teobot/bucket/blob/main/portfolio/typeincat.png?raw=true",
       github: "https://github.com/teobot/typeInCat",
       live: "https://type-in-cat.netlify.app/",
+      order: 9,
     },
     {
       title: "Train Station Finder",
       desc: "Produced during a 2 days Hackathon, this application allows you to find the nearest train station to your current location.",
       img: "https://github.com/teobot/bucket/blob/main/portfolio/train-station-app.png?raw=true",
       github: "https://github.com/teobot/Android-Client-Server-App",
-      live: "",
+      order: 8,
     },
     {
       title: "GameGuide",
       desc: "This is a computer game review website. This project uses the PHP, more specifically the framework called CodeIgniter and follows the MVC framework. It also includes a Node.Js server that allows users to chat to each other in real time.",
       img: "https://github.com/teobot/bucket/blob/main/portfolio/GameGuide-min.png?raw=true",
       github: "https://github.com/teobot/GameGuide",
-      live: "",
+      order: 7,
     },
     {
       title: "Java Web Service API",
       desc: "This is a web service API that implement all CRUD operations, written in Java. It showcases the use of the Java Servlet API.",
       img: "https://github.com/teobot/bucket/blob/main/portfolio/Javaweb-min.png?raw=true",
       github: "https://github.com/teobot/Enterprise-Programming-Web-Service",
-      live: "",
+      order: 7,
     },
     {
       title: "Aircraft Scheduler",
       desc: "This project showcases key components of a system that can plan which plane an airline uses to fly passengers on each of their flights, and choose which pilots and cabin crew are rostered for each flight",
       img: "https://github.com/teobot/bucket/blob/main/portfolio/aircraftsceduler-min.png?raw=true",
       github: "https://github.com/teobot/Airline-Scheduling-System",
-      live: "",
+      order: 8,
     },
     {
       title: "Pokemon Sprite Timeline",
@@ -166,6 +164,7 @@ export async function getServerSideProps(context) {
       img: "https://github.com/teobot/bucket/blob/main/portfolio/pokemonBanner.png?raw=true",
       github: "https://github.com/teobot/pokemon-sprite-changes",
       live: "https://pokemon-versions.netlify.app/",
+      order: 8,
     },
   ];
 

577d4aaba80f10143bc39146eaad82691d9498d0
Theo Clapperton <theoclapperton@outlook.com>
2022-08-22 14:38:46 +0100

    improved SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Head from "next/head";
-
 import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Theo Clapperton | Portfolio</title>
+        <meta
+          key="desc"
+          name="description"
+          content="Theo Clapperton's portfolio website."
+        />
+        <meta property="og:title" content="Theo Clapperton | Portfolio" />
+        <meta
+          property="og:description"
+          content="Theo Clapperton's portfolio website."
+        />
         <link
           key="hack-font"
           rel="stylesheet"
